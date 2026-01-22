6601705d8ba8fbd66ae89f65c78dc4dc6012a2af
arieyuval <arieyuval33@gmail.com>
2026-01-22 15:40:47 -0800

    capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yuvalarie.plates',
+  appName: 'Plates',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
